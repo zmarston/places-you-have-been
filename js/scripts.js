eaf19d947625a7d5726e6b2a9c0b6926d254ff8e
Mikey Pinaud and Zachary Marston <zachary@>
2017-10-30 11:02:00 -0700

    add js to display place list

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,29 @@
 //business logic
+function Location(place, when, who, info) {
+  this.place = place;
+  this.when = when;
+  this.who = who;
+  this.info = info;
+}
 
 //user-interface-logic
 $(function(){
-  $("form-new-place").submit(function(event){
-    event.preventDefault;
+  $("form#new-place").submit(function(event){
+    event.preventDefault();
 
+    var inputtedPlace = $("input#place-input").val();
+    var inputtedWhen = $("input#when-input").val();
+    var inputtedWho = $("input#who-input").val();
+    var inputtedInfo = $("input#info-input").val();
+
+    var newDestination = new Location(inputtedPlace, inputtedWhen, inputtedWho, inputtedInfo);
+
+    $("ul#destination").append("<li><span class='place-added'>" + newDestination.place + "</span></li>");
+
+    $("input#place-input").val("");
+    $("input#when-input").val("");
+    $("input#who-input").val("");
+    $("input#info-input").val("");
 
   });
 });
